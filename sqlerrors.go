0fc9d08796a2a566b4a6e3476393fa04aed6e549
Gabriel Ochsenhofer <gabs@gabs.tv>
2021-07-16 15:43:26 -0300

    basic errors

---
+++ sqlerrors.go
@@ -0,0 +1,32 @@
+package protodb
+
+type QueryError struct {
+	Query string
+	Err   error
+}
+
+func (e *QueryError) Error() string { return e.Query + ": " + e.Err.Error() }
+func (e *QueryError) Unwrap() error { return e.Err }
+
+type NotFoundError struct {
+	Name string
+}
+
+func (e *NotFoundError) Error() string { return e.Name + ": not found" }
+
+// IsNotFound tests if an error is a *NotFoundError
+func IsNotFound(err error) bool {
+	if err == nil {
+		return false
+	}
+	if _, ok := err.(*NotFoundError); ok {
+		return true
+	}
+	return false
+}
+
+func NotFound(name string) error {
+	return &NotFoundError{
+		Name: name,
+	}
+}
